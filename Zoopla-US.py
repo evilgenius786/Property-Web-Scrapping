85bccdda68ce12e01f797dea380d17d7816aba23
Muhammad Hassan <786hassan777@gmail.com>
2021-11-09 10:26:24 +0500

    Added SQL DB integration
    First it scrapes all data into CSV file and then insert all into DB at once to save memory and CPU overhead!

--- Zoopla-US.py
+++ Zoopla-US.py
@@ -1,58 +1,28 @@
 import csv
+import os
 import threading
 import traceback
 
-import json
-import os
-
 import cfscrape
+import json
+import pymysql
 from bs4 import BeautifulSoup
+from pymysql.converters import escape_string
+
+DB_HOST = "localhost"
+DB_USER = 'root'
+DB_PW = ''
+DB_NAME = 'ZooplaUS'
+TABLE_NAME = 'ZooplaUS'
+
 
 semaphore = threading.Semaphore(1)
 write = threading.Semaphore(1)
-outcsv = "out.csv"
-errorfile = "error.txt"
-headers = ["Name", "Price (EUR)", "Price (USD)", "Price/Area", "Location", "Contact", "Description", "URL", "Features",
+outcsv = "Out-Zoopla-US.csv"
+errorfile = "Error-Zoopla-US.txt"
+headers = ["Name", "PriceEUR", "PriceUSD", "PricePerArea", "Location", "Contact", "Description", "URL", "Features",
            "Images"]
 scraped = []
-
-
-def scrape(url):
-    with semaphore:
-        try:
-            print("Working on", url)
-            soup = get(url)
-            if "403 Forbidden" in soup.text:
-                print("403 Forbidden", url)
-                return
-            data = {
-                "Name": soup.find('title').text,
-                "Price (EUR)": int(getText(soup, 'p', "ui-pricing__main-price ui-text-t4")[1:].replace(",", "")),
-                "Price (USD)": int(getText(soup, 'p', "ui-pricing__alt-price")[3:].replace(",", "")),
-                "Price/Area": getText(soup, 'p', 'ui-pricing__area-price').replace("(", "").replace(")", ""),
-                "Location": getText(soup, 'h2', 'ui-property-summary__address'),
-                "Contact": getText(soup, 'p', 'ui-agent__tel ui-agent__text').split("  ")[-1],
-                "Description": getText(soup, 'div', 'dp-description__text'),
-                "URL": url,
-                "Features": [li.text.strip() for li in soup.find_all('li', {"class": "dp-features-list__item"})],
-                "Images": [img['src'] for img in soup.find_all('img', {'class': 'dp-gallery__image'})],
-            }
-            print(json.dumps(data, indent=4))
-            with open(f"./json/{url.split('/')[-2]}.json", 'w', encoding='utf8', errors='ignore') as outfile:
-                json.dump(data, outfile, indent=4)
-            append(data)
-            scraped.append(url.split("/")[-2])
-        except:
-            print("Error on", url)
-            with open(errorfile, 'a') as efile:
-                efile.write(url + "\n")
-            traceback.print_exc()
-
-
-def append(data):
-    with write:
-        with open(outcsv, 'a', newline="", encoding='utf8', errors='ignore') as outfile:
-            csv.DictWriter(outfile, fieldnames=headers).writerow(data)
 
 
 def main():
@@ -100,6 +70,51 @@ def main():
         hope_soup = get(start_url)
     for thread in threads:
         thread.join()
+    print("Done with scraping, now adding stuff to DB.")
+    handler = DBHandler()
+    with open(outcsv) as outfile:
+        rows = [row for row in csv.DictReader(outfile)]
+        handler.bulkInsert(rows)
+    print("Done with DB insertion!")
+
+
+def scrape(url):
+    with semaphore:
+        try:
+            print("Working on", url)
+            soup = get(url)
+            if "403 Forbidden" in soup.text:
+                print("403 Forbidden", url)
+                return
+            data = {
+                "Name": soup.find('title').text,
+                "PriceEUR": int(getText(soup, 'p', "ui-pricing__main-price ui-text-t4")[1:].replace(",", "")),
+                "PriceUSD": int(getText(soup, 'p', "ui-pricing__alt-price")[3:].replace(",", "")),
+                "PricePerArea": getText(soup, 'p', 'ui-pricing__area-price').replace("(", "").replace(")", ""),
+                "Location": getText(soup, 'h2', 'ui-property-summary__address'),
+                "Contact": getText(soup, 'p', 'ui-agent__tel ui-agent__text').split("  ")[-1],
+                "Description": getText(soup, 'div', 'dp-description__text'),
+                "URL": url,
+                "Features": " | ".join(
+                    [li.text.strip() for li in soup.find_all('li', {"class": "dp-features-list__item"})]),
+                "Images": " | ".join([img['src'] for img in soup.find_all('img', {'class': 'dp-gallery__image'})]),
+            }
+            print(json.dumps(data, indent=4))
+            with open(f"./json/{url.split('/')[-2]}.json", 'w', encoding='utf8', errors='ignore') as outfile:
+                json.dump(data, outfile, indent=4)
+            append(data)
+            scraped.append(url.split("/")[-2])
+        except:
+            print("Error on", url)
+            with open(errorfile, 'a') as efile:
+                efile.write(url + "\n")
+            traceback.print_exc()
+
+
+def append(data):
+    with write:
+        with open(outcsv, 'a', newline="", encoding='utf8', errors='ignore') as outfile:
+            csv.DictWriter(outfile, fieldnames=headers).writerow(data)
 
 
 def getText(soup, tag, Class):
@@ -113,9 +128,102 @@ def get(url):
     return BeautifulSoup(cfscrape.create_scraper().get(url).text, 'lxml')
 
 
+class DBHandler:
+    DB_CONN = None
+    scraped = []
+
+    def __init__(self):
+        self.DB_HOST = DB_HOST
+        self.DB_USER = DB_USER
+        self.DB_PW = DB_PW
+        self.DB_NAME = DB_NAME
+        self.TABLE_NAME = TABLE_NAME
+        self.openConnection()
+        self.setupDB()
+
+    def setupDB(self):
+        self.createDB()
+        self.DB_CONN.select_db(self.DB_NAME)
+        self.createTable()
+
+    def openConnection(self):
+        self.DB_CONN = pymysql.connect(host=self.DB_HOST, user=self.DB_USER, password=self.DB_PW, autocommit=True)
+
+    def closeConnection(self):
+        self.DB_CONN.close()
+
+    def createDB(self):
+        self.executeSQL(f"CREATE DATABASE IF NOT EXISTS {self.DB_NAME}")
+
+    def executeSQL(self, sql, args=None):
+        if self.DB_CONN is None:
+            print("Please open connection first!")
+            return
+        with self.DB_CONN.cursor(pymysql.cursors.DictCursor) as cursor:
+            cursor.execute(sql, args)
+            queryResult = cursor
+        return queryResult
+
+    def exists(self, url):
+        sql = f"SELECT COUNT(*) FROM {self.TABLE_NAME} WHERE URL='{url}'"
+        res = self.executeSQL(sql).fetchone()['COUNT(*)']
+        if res == 0:
+            return False
+        else:
+            return True
+
+    def getInt(self, value):
+        try:
+            return int(value)
+        except Exception:
+            return 0
+
+    def createTable(self):
+        self.executeSQL(f"""CREATE TABLE IF NOT EXISTS {self.TABLE_NAME} (
+        Name TEXT, 
+        PriceEUR INTEGER, 
+        PriceUSD INTEGER,
+        PricePerArea TEXT,
+        Location TEXT,
+        Contact TEXT,
+        Description Text,
+        URL Text,
+        Features Text,
+        Images Text
+        )""")
+
+    def createQuery(self, data):
+        return f"""INSERT INTO {self.TABLE_NAME} (Name, PriceEUR, PriceUSD, PricePerArea, Location,Contact, Description,
+         URL, Features, Images) VALUES 
+        ('{escape_string(data["Name"])}','{data["PriceEUR"]}','{data["PriceUSD"]}','{data["PricePerArea"]}',
+        '{escape_string(data["Location"])}','{data["Contact"]}','{escape_string(data["Description"])}',
+        '{escape_string(data["URL"])}','{escape_string(data["Features"])}','{data["Images"]}');"""
+
+    def insert(self, data):
+        if not self.exists(data['URL']):
+            self.executeSQL(self.createQuery(data))
+            print(data['URL'], "inserted!")
+        else:
+            print(data['URL'], 'already in db!')
+
+    def bulkInsert(self, rows):
+        self.scraped = [row['URL'] for row in self.getAllData()]
+        print("Already scraped", self.scraped)
+        for row in rows:
+            if row['URL'] not in self.scraped:
+                self.executeSQL(self.createQuery(row))
+                print(row['URL'], "inserted!")
+            else:
+                print(row['URL'], "already exists!")
+
+    def getAllData(self):
+        sql = f"SELECT * FROM {self.TABLE_NAME}"
+        return self.executeSQL(sql).fetchall()
+
+
 def logo():
     os.system('color 0a')
-    print(f"""
+    print(rf"""
         __________                   .__          
         \____    /____   ____ ______ |  | _____   
           /     //  _ \ /  _ \\\\____ \|  | \__  \  
